7e5e55807531523e3523db6651591ac2c7cd4699
EDWARD YANG <eddiecody888@gmail.com>
2018-12-13 04:32:35 +0000

    error fix, all tweeter added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,26 +3,88 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-function createTweetElement(){
-  let $tweet = $("<article>").addClass("tweet");
-  let $header = $("<header>");
-  let $img = $("img").atrr("src",tweetData.user.avatars.small);
-  let $author = $ ("author").text(tweetData.user.name);
-  let $handler = $ ("handler").text(tweetData.user.handler);
-  let $littleTweet = $("<p>").text(tweetData.content.text);
-  let $footer      = $("<footer>");
+$(document).ready(function() {
 
-  $('#tweets-container').append($tweet);
-  $tweet.append($header);
-  $header.append($img);
-  $header.append($author);
-  $header.append($handler);
-  $tweetBody.append($littleTweet);
-  $tweet.append($footer);
+  function createTweetElement (data){
+    let tweetAll = `
+         <article class="tweets">
+          <header>
+            <img src= ${data.user.avatars.small} />
+            <span class="author">${data.user.name}</span>
+            <span class="handler">${data.user.handle}</span>
+          </header>
+          <div class="tweet-body">${data.content.text}</div>
+          <footer>
+            <span>x days ago</span>
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </footer>
+        </article>
+       `;
+    return $(tweetAll);
+  };
 
-   return $tweet;
-};
+
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
 
 
+   function renderTweets(tweets){
+    for (let tweetData of tweets) {
+      const $tweet = createTweetElement(tweetData);
+      $(".container").append($tweet);
+    }
 
+   };
+
+   renderTweets(data);
+
+
+});
